e88b99f334e422413c0e348f138dacbb65fe9db2
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-06-08 21:52:11 +0530

    Adding Appbar

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,26 @@
+
+
+export const AppBar = () =>{
+    return <div className="border-b flex justify-center mx-auto w-80 py-4 bg-white rounded-full mt-4 space-x-4 ">
+    <nav className="flex justify-center space-x-4">
+      <a href="#home" className="text-gray-500 hover:text-black px-3 py-2 rounded-md text-sm font-medium">
+        Home
+      </a>
+      <a href="#about" className="text-gray-500 hover:text-black px-3 py-2 rounded-md text-sm font-medium">
+        About
+      </a>
+      <a href="#projects" className="text-gray-500 hover:text-black px-3 py-2 rounded-md text-sm font-medium">
+        Projects
+      </a>
+      <a href="#skills" className="text-gray-500 hover:text-black px-3 py-2 rounded-md text-sm font-medium">
+        Skills
+      </a>
+      <a href="#experience" className="text-gray-500 hover:text-black px-3 py-2 rounded-md text-sm font-medium">
+        Experience
+      </a>
+      <a href="#contact" className="text-gray-500 hover:text-black px-3 py-2 rounded-md text-sm font-medium">
+        Contact
+      </a>
+    </nav>
+  </div>
+}
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,8 @@
+import { AppBar } from "../components/AppBar"
+
+export const LandingPage = () => {
+    return <div className="bg-pink-200">
+        <AppBar/>
+
+    </div>
+}
