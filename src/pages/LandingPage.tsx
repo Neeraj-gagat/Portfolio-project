ca6b4ef77170d83068215b7cb4390b5d9b10f113
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-06-11 21:26:55 +0530

    adding intro

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,8 @@
 import { AppBar } from "../components/AppBar"
 
 export const LandingPage = () => {
-    return <div className="bg-pink-200">
-        <AppBar/>
+    return <div className="bg-pink-200 min-h-screen">
+        <div className="pt-2"> <AppBar/> </div>
 
     </div>
 }
