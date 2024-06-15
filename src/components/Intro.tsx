5f7d815549ab43265ed16c1006dfc79b5a13f2b5
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-06-15 22:27:06 +0530

    adding motion animatiion

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,7 +1,16 @@
+import  { WordsFlip }  from "./ui/WordsFlip";
 
 
 export const Intro = () => {
-    return <div className="justify-center content-center flex text-neutral-950 text-xl ">
-        Hello, I'm Neeraj. I'm a TechEnthusiast with 2 years of experience. I enjoy building websites. My focus is React (Next.js).
+    const words = ["Full Stack Developer", "Software Engineer", "MERN Stack Developer", "Tech Enthusiast", "NextJS Developer"];
+
+    return <div className="p-8 content-center flex text-neutral-950 text-2xl text-center mt-10 ">
+        <div className="justify-center content-around">
+            <span className="font-bold">Hello, I&apos;m Neeraj.</span> I&apos;m a
+            <span className="font-bold"><WordsFlip className="!text-black" duration={2000} words={words} /></span> with
+            <span className="font-bold">2 years</span> of experience. I enjoy
+            building <span className="italic">websites</span>. My focus is
+            <span className="underline">React (Next.js)</span>.
+        </div>
     </div>
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,7 +4,7 @@ import { Intro } from "../components/Intro"
 export const LandingPage = () => {
     return <div className="bg-pink-200 min-h-screen">
         <div className="pt-2"> <AppBar/> </div>
-        <Intro/>
+        <div><Intro/></div>
 
     </div>
 }
