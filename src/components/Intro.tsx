4cf0faf69c5f546862fbc999af56bb92e3aa9f5b
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-06-15 11:16:21 +0530

    adding intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,7 +1,7 @@
 
 
 export const Intro = () => {
-    return <div>
-        
+    return <div className="justify-center content-center flex text-neutral-950 text-xl ">
+        Hello, I'm Neeraj. I'm a TechEnthusiast with 2 years of experience. I enjoy building websites. My focus is React (Next.js).
     </div>
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,10 @@
 import { AppBar } from "../components/AppBar"
+import { Intro } from "../components/Intro"
 
 export const LandingPage = () => {
     return <div className="bg-pink-200 min-h-screen">
         <div className="pt-2"> <AppBar/> </div>
+        <Intro/>
 
     </div>
 }
