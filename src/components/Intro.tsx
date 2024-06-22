855204fd26de0b99277dd98b35aac9a2dee71bf6
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-06-22 18:39:42 +0530

    adding icons in intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,19 @@
 import  { WordsFlip }  from "./ui/WordsFlip";
+import { GrGithub } from "react-icons/gr";
+import { FaLinkedin } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+
+
+
 
 
 export const Intro = () => {
     const words = ["Full Stack Developer", "Software Engineer", "MERN Stack Developer", "Tech Enthusiast", "NextJS Developer"];
 
-    return <div className="content-center flex text-neutral-950 text-4xl text-center">
+    return <div className="content-center  text-neutral-950 text-4xl text-center">
+        <div className="flex justify-center pt-10">
+            <CgProfile className="size-32"/>
+            </div>
         <div className="flex items-center justify-center pt-10 mx-auto">
             <div>
             <span className="font-bold">Hello, I&apos;m Neeraj.</span> I&apos;m a
@@ -17,6 +26,12 @@ export const Intro = () => {
             <span className="italic">websites</span>. My focus is
             <span className="underline">React (Next.js)</span>.
             </div>
+        </div>
+        <div className="flex justify-center space-x-8 mt-16">
+            <button className="bg-black text-white px-7 py-3 text-xl rounded-3xl">Contact Me Here</button>
+            <button className=" text-black bg-slate-100 px-7 py-3 text-xl rounded-3xl">Downlod cv</button>
+            <div><GrGithub /> </div>
+            <div><FaLinkedin /></div>
         </div>
     </div>
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import { AppBar } from "../components/AppBar"
 import { Intro } from "../components/Intro"
 
 export const LandingPage = () => {
-    return <div className="bg-pink-200 min-h-screen">
+    return <div className="bg-pink-100 min-h-screen">
         <div className="pt-2"> <AppBar/> </div>
         <Intro/>
 
