0045af6da9f7056d8af07c41c804f3d5679c1c9d
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-07-23 21:02:41 +0530

    fixes

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -11,12 +11,12 @@ import profile from "../../public/profilepic.jpg"
 export const Intro = () => {
     const words = ["Full Stack Developer", "Software Engineer", "MERN Stack Developer", "Tech Enthusiast", "NextJS Developer"];
 
-    return <div className="content-center bg-gradient-to-b from-purple-100 via-pink-100 to-white  text-neutral-950 text-4xl text-center" id="home">
+    return <div className="content-center bg-gradient-to-b from-purple-200 via-pink-100 to-white  text-neutral-950 text-4xl text-center" id="home">
         <div className="flex justify-center pt-36">
                 <img src={profile} className="size-28 rounded-full border-8 border-white" alt="image is not available" />
             </div>
         <div className="flex items-center justify-center pt-5 mx-auto">
-            <div className=" space-y-4">
+            <div className="space-x-4">
             <div>
                 <span className="font-bold font-serif">Hello, I&apos;m Neeraj.</span> I&apos;m a
                 <span className="font-bold"><WordsFlip className="!text-black" duration={2000} words={words} /></span>
@@ -32,11 +32,11 @@ export const Intro = () => {
             </div>
             </div>
         </div>
-        <div className="flex justify-center space-x-8 mt-10">
-            <button className="focus:scale-[1.15] hover:scale-[1.15] transition bg-gray-900 hover:bg-black text-white px-7 py-3 text-xl rounded-full shadow-xl flex">Contact Me Here
+        <div className="flex flex-col justify-center gap-[1.3rem] px-4 sm:flex-row items-center text-lg font-medium mx-11 mt-10">
+            <button className="focus:scale-[1.10] hover:scale-[1.10] transition bg-gray-900 hover:bg-black text-white gap-2 px-7 py-3 text-lg rounded-full shadow-xl flex">Contact Me Here
                  <div className="flex pt-1 size-8 text-white pl-2"><BsArrowRight/></div>
             </button>
-            <button className="focus:scale-[1.15] hover:scale-[1.15] transition hover:bg-gray-200 active:scale-105 text-black bg-slate-100 px-7 py-3 text-xl border rounded-full shadow-xl flex">Download CV
+            <button className="focus:scale-[1.10] hover:scale-[1.10] transition hover:bg-gray-200 active:scale-105 text-black bg-slate-100 px-7 py-3 text-lg border rounded-full shadow-xl flex">Download CV
                 <div className="pt-1 text-gray-500 mx-1"><HiDownload/></div>
             </button>
             <div className="group border-black/10 p-4 items-center text-[1.35rem] shadow-xl rounded-full flex gap-2 focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 hover:bg-gray-200 active:scale-105 transition cursor-pointer border" ><GrGithub /> </div>
