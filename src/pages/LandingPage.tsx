de1962eb345d5d68a1b2762c2cde8e18cb9b5cd8
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-02-01 00:00:13 +0530

    snowflakes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -12,8 +12,8 @@ import Snowfall from 'react-snowfall'
 export const LandingPage = () => {
     return <div className="relative bg-gradient-to-br from-slate-950 via-black to-blue-950 overflow-hidden">
         <div className="fixed inset-0 pointer-events-none z-50">
-           <Snowfall color="pink" snowflakeCount={40}/>
-           <Snowfall color="#FFB7C5" snowflakeCount={40}/>
+           <Snowfall color="white" snowflakeCount={30}/>
+           <Snowfall color="blue" snowflakeCount={30}/>
         </div>
         <AppBar/>
         <Intro/>
