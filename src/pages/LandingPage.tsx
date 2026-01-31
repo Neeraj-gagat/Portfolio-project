1f3421136569f239c507927bc038c65839bb04fa
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-01-31 23:54:55 +0530

    snowflakes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,9 +7,14 @@ import { Intro } from "../components/Intro"
 import { Projects } from "../components/Projects"
 import SectionDivider from "../components/SectionDevider"
 import { Skills } from "../components/Skills"
+import Snowfall from 'react-snowfall'
 
 export const LandingPage = () => {
-    return <div className="bg-gradient-to-br from-slate-950 via-black to-blue-950 overflow-hidden">
+    return <div className="relative bg-gradient-to-br from-slate-950 via-black to-blue-950 overflow-hidden">
+        <div className="fixed inset-0 pointer-events-none z-50">
+           <Snowfall color="pink" snowflakeCount={40}/>
+           <Snowfall color="#FFB7C5" snowflakeCount={40}/>
+        </div>
         <AppBar/>
         <Intro/>
         <SectionDivider/>
@@ -19,6 +24,5 @@ export const LandingPage = () => {
         <Experience/>
         <Contributions/>
         <ContactMe/>
-        
     </div>
 }
