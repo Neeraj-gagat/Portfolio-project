b2f4c3566fc8bc61707fd16ea551b49f095fa77b
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-02-10 03:27:15 +0530

    lag fix on mobilescreen

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,84 +1,36 @@
 import { motion } from "framer-motion";
+import { skillsarr } from "../data/data";
 
 
 export const Skills = () => {
-    const skillsarr:string[] = [
-  "Git",
-  "Zod",
-  "JWT",
-  "SQL",
-  "HTML",
-  "CSS",
-  "AWS",
-  "Bun",
-  "Hono",
-  "noSQL",
-  "Recoil",
-  "Redux",
-  "CI/CD",
-  "Docker",
-  "Redis",
-  "RabbitMQ",
-  "kafka",
-  "Next.js",
-  "Node.js",
-  "MongoDB",
-  "Grafana",
-  "GraphQL",
-  "Prisma",
-  "TypeScript",
-  "React.js",
-  "JavaScript",
-  "Kubernetes",
-  "TurboREPO",
-  "Prometheus",
-  "Express.JS",
-  "REST APIs",
-  "Algorithms",
-  "Tailwind CSS",
-  "PostgreSQL",
-  "Framer Motion",
-  "Data Structures",
-  "Serverless Backends",
-  "OpenAPI Specification",
-  "Clerk Authentication",
-  "Cloudflare Workers",
-  "Database Optimization",];
-
-  const fadeInAnimationVariants = {
-    initial: { opacity: 0, y: 100 },
-    animate: (index: number) => ({
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.3,
-        delay: 0.03 * index,
-      },
-    }),
-  };
     
-    return <div id="skills" className="flex justify-center bg-transparent z-10">
+    return <motion.div 
+    initial={{ opacity: 0, y: 100 , filter: 'blur(10px)'}}
+    whileInView={{
+                opacity: 1,
+                y:0,
+                filter: 'blur(0px)'
+                }}
+                transition={{
+                duration: 0.5,
+                delay:0.3
+                }}
+    viewport={{ once: true, amount:0.3 }}
+    id="skills" className="flex justify-center bg-transparent z-10">
                 <div className="mb-28 justify-center max-w-[53rem] scroll-mt-28 text-center sm:mb:40">
                     <div className="text-3xl text-white font-serif"> My Skills</div>
                         <div className=" mt-12 w-[350px] md:w-[610px] text-justify">
                             <ul className="flex flex-wrap justify-center gap-1.5 text-xs md:text-sm uppercase font-medium text-white">
                                 {skillsarr.map((skill, index) => (
-                                    <motion.li
+                                    <li
                                     className="bg-white/10 backdrop-blur-[0.2rem] border-black/[0.1] rounded-lg px-3 py-1 hover:text-black hover:bg-zinc-300 transition-colors duration-200 cursor-default"
                                     key={index}
-                                    variants={fadeInAnimationVariants}
-                                    initial="initial"
-                                    whileInView="animate"
-                                    viewport={{
-                                    once: true,
-                                    }}
-                                    custom={index}
                                     >
                                         {skill}
-                                    </motion.li>
+                                    </li>
                                 ))}
                             </ul>
                         </div>
                 </div>
-            </div>
+            </motion.div>
 }
